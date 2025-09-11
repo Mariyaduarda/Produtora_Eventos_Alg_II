b6b7a1f2314b888310c5007c8d4f3a4d42657ead
VICTOR LUIS ARAUJO LIMA <0076970@ifmg>
2025-09-11 14:50:33 -0300

    Implementação dos menus principais do CRUD

--- view/menu.c
+++ view/menu.c
@@ -3,44 +3,82 @@
 // Aqui tem tds as funcoes de exibicao de menus
 
 void printMenuPrincipal(){
-    printf("=== Menu Principal ===\n"
-        "1. Gerenciar Clientes\n"
-        "2. Gerenciar Fornecedores e Parceiros\n"
-        "3. Gerenciar Produtora\n"
-        "4. Gerenciar Equipe Interna\n"
-        "5. Gerenciar Recursos e Equipamentos\n"
-        "6. Gerenciar Operadores do Sistema\n"
-        "0. Sair\n"
+    printf("\n => Menu Principal"
+        "\n 1. Gerenciar Clientes                 "
+        "\n 2. Gerenciar Fornecedores e Parceiros "
+        "\n 3. Gerenciar Produtora                "
+        "\n 4. Gerenciar Equipe Interna           "
+        "\n 5. Gerenciar Recursos e Equipamentos  "
+        "\n 6. Gerenciar Operadores do Sistema    "
+        "\n 0. Sair                               "
     );
 }
 
 void printMenuClientes(){
-    printf("=== Menu Clientes ===\n"
-        "1. Adicionar Cliente\n"
-        "2. Remover Cliente\n"
-        "3. Atualizar Cliente\n"
-        "4. Buscar Cliente por ID\n"
-        "5. Listar Todos os Clientes\n"
-        "0. Voltar ao Menu Principal\n"
+    printf("\n => Clientes"
+        "\n 1. Adicionar"
+        "\n 2. Remover"
+        "\n 3. Atualizar"
+        "\n 4. Buscar"
+        "\n 5. Listar"
+        "\n 0. Voltar "
     );
 }
 
 void printMenuFornecedoresParceiros(){
-
+    printf("\n => Fornecedores & Parceiros"
+        "\n 1. Adicionar"
+        "\n 2. Remover"
+        "\n 3. Atualizar"
+        "\n 4. Buscar"
+        "\n 5. Listar"
+        "\n 0. Voltar "
+    );
 }
 
 void printMenuProdutora(){
+    printf("\n => Produtora"
+        "\n 1. Adicionar"
+        "\n 2. Remover"
+        "\n 3. Atualizar"
+        "\n 4. Buscar"
+        "\n 5. Listar"
+        "\n 0. Voltar "
+    );
 
 }
 
 void printMenuEquipeInterna(){
+    printf("\n => Equipe Interna"
+        "\n 1. Adicionar"
+        "\n 2. Remover"
+        "\n 3. Atualizar"
+        "\n 4. Buscar"
+        "\n 5. Listar"
+        "\n 0. Voltar "
+    );
 
 }
 
 void printMenuRecursosEquipamentos(){
+    printf("\n => Recursos & Equipamentos"
+        "\n 1. Adicionar"
+        "\n 2. Remover"
+        "\n 3. Atualizar"
+        "\n 4. Buscar"
+        "\n 5. Listar"
+        "\n 0. Voltar "
+    );
 
 }
 
 void printMenuOperadoresSistema(){
-
+    printf("\n => Operadores do Sistema"
+        "\n 1. Adicionar"
+        "\n 2. Remover"
+        "\n 3. Atualizar"
+        "\n 4. Buscar"
+        "\n 5. Listar"
+        "\n 0. Voltar "
+    );
 }
