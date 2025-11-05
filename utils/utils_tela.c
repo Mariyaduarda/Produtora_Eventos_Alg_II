84bd5ba7d6b99864abe36615c2e5c0ab55de6e40
Victor Luis <victor.rbread@gmail.com>
2025-11-05 16:28:02 -0300

    funções de formatação lidam melhor com erros

--- utils/utils_tela.c
+++ utils/utils_tela.c
@@ -16,14 +16,24 @@ void esperaEnter() {
 
 void formataCPF(char *cpf, char *dest) {
     // formata a string como se fosse um cpf
-    //  presupoe que ja e' um cpf
-    sprintf(dest, "%.3s.%.3s.%.3s-%.2s", cpf, cpf + 3, cpf + 6, cpf + 9);
+    //  presupoe que ja e' so' numeros
+
+    if (strlen(cpf) == 11){ // se tem o num certo de caracteres
+        sprintf(dest, "%.3s.%.3s.%.3s-%.2s", cpf, cpf + 3, cpf + 6, cpf + 9);
+    } else{ // caso contrario, so' retorna ele normal
+        strcpy(dest, cpf);
+    }
 }
 
 void formataCNPJ(char *cnpj, char *dest) {
     // formata a string como se fosse um cnpj
-    //  presupoe que ja e' um cnpj
-    sprintf(dest, "%.2s.%.3s.%.3s/%.4s-%.2s", cnpj, cnpj + 2, cnpj + 5, cnpj + 8, cnpj + 12);
+    //  presupoe que ja e' so' numeros
+
+    if (strlen(cnpj) == 11){ // se tem o num certo de caracteres
+        sprintf(dest, "%.2s.%.3s.%.3s/%.4s-%.2s", cnpj, cnpj + 2, cnpj + 5, cnpj + 8, cnpj + 12);
+    } else{ // caso contrario, so' retorna ele normal
+        strcpy(dest, cnpj);
+    }
 }
 
 void formataTelefone(char *numero, char *dest) {
@@ -31,19 +41,22 @@ void formataTelefone(char *numero, char *dest) {
     //  presupoe que ja e' so' numeros
 
     if (strlen(numero) == 11) {
-        // 11987654321 vira (11) 98765-4321
-        sprintf(dest, "(%.2s) %.5s-%.4s", numero, numero+2, numero+7);
+        // 12345678901 vira (12) 34567-8901
+        sprintf(dest, "(%.2s) %.5s-%.4s", numero, numero + 2, numero + 7);
     } 
     else if (strlen(numero) == 10) {
-        // 1134567890 vira (11) 3456-7890
-        sprintf(dest, "(%.2s) %.4s-%.4s", numero, numero+2, numero+6);
+        // 1234567890 vira (12) 3456-7890
+        sprintf(dest, "(%.2s) %.4s-%.4s", numero, numero + 2, numero + 6);
+    }
+    else if (strlen(numero) == 9) {
+        // 123456789 vira 12345-6789
+        sprintf(dest, "%.5s-%.4s", numero, numero + 5);
     }
     else if (strlen(numero) == 8) {
-        // 34567890 vira 3456-7890
-        sprintf(dest, "%.4s-%.4s", numero, numero+4);
+        // 12345678 vira 1234-5678
+        sprintf(dest, "%.4s-%.4s", numero, numero + 4);
     }
-    else {
-        // se n segue nenhum desses padroes, so copia a original
+    else { // caso contrario, so' retorna ele normal
         strcpy(dest, numero);
     }
 }
